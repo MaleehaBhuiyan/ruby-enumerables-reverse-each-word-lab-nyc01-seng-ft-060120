35b1a45d10a71ed8d0cb4e6d6e54e645f194dcba
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-05-20 15:50:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   original_string_array = string.split(/ /)
   revered_array = []
-  original_string_array.collect.join(" ") do |word|
+  original_string_array.collect do |word|
     word.reverse
   end
 end
